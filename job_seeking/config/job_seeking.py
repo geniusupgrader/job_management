1b94b0408898e293fa9867ae5cab62c0533b2aba
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-08-21 19:46:40 +0200

    desk and changes

--- job_seeking/config/job_seeking.py
+++ job_seeking/config/job_seeking.py
@@ -9,8 +9,26 @@ def get_data():
 				{
 					"type": "doctype",
 					"name": "job_application_job_seeking",
-					"description":_("Job Application"),
-					"onboard": 1,
+					"label": _("Job Application"),
+					"description": _("Job Application"),
+				},
+				{
+					"type": "doctype",
+					"name": "application_method_job_seeking",
+					"label": _("Application Method"),
+					"description": _("Application Method"),
+				},
+				{
+					"type": "doctype",
+					"name": "Job_Classification_job_seeking",
+					"label": _("Job Classification"),
+					"description": _("Job Classification"),
+				},
+				{
+					"type": "doctype",
+					"name": "Text_Template_job_seeking",
+					"label": _("Text Template"),
+					"description": _("Text Template"),
 				}
 			]
 		}
