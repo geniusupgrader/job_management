7f0489c8f2b1226af6858661ac1b78d479f40bab
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-11-15 23:05:29 +0100

    created cv with symbols begin

--- job_management/job_management/doctype/symbols_and_images_job_management/symbols_and_images_job_management.py
+++ job_management/job_management/doctype/symbols_and_images_job_management/symbols_and_images_job_management.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class Curriculum_vitae_job_management(Document):
-		pass
+class symbols_and_images_job_management(Document):
+	pass
