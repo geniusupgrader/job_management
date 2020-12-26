15351b83feb44c8bba86c826ffce075eef393782
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-12-26 10:06:41 +0100

    created first working cv in erpnext, but there is much to clean...

---
+++ job_management/job_management/doctype/cv_work_history_job_template_management/cv_work_history_job_template_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Robin Rosenstock and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('CV_work_history_job_template_management', {
+	// refresh: function(frm) {
+
+	// }
+});
